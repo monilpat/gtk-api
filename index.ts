51e88cbf899559c93f78f300e206a6771c6c7558
Monil Patel <monilpat@gmail.com>
2024-06-30 16:58:15 -0700

    fix typing issues

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@
 export { APIClientWrapper } from "./src/api/APIClientWrapper";
 export type { IAPIClient } from "./src/api/IAPIClient";
 export {
-  TargetTokenType,
-  CollateralTokenType,
   PnlTypeEnum,
   TradeDirectionEnum,
   TradeStatusEnum,
+} from "./src/api/types";
+export type {
+  TargetTokenType,
+  CollateralTokenType,
   Trade,
 } from "./src/api/types";
 export { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
