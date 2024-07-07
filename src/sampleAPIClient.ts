a315a2fa234b979790cf8c3856f24fe30c57406e
Monil Patel <monilpat@gmail.com>
2024-07-06 20:07:36 -0700

    update structure and run tsc

--- src/sampleAPIClient.ts
+++ src/sampleAPIClient.ts
@@ -1,25 +1,25 @@
-import {
-  APIClientWrapper,
-  PnlTypeEnum,
-  TradeDirectionEnum,
-  TradeStatusEnum,
-  TargetTokenType,
-  CollateralTokenType,
-  DirectSecp256k1HdWallet,
-} from "gtk-api";
+// import {
+//   APIClientWrapper,
+//   PnlTypeEnum,
+//   TradeDirectionEnum,
+//   TradeStatusEnum,
+//   TargetTokenType,
+//   CollateralTokenType,
+//   DirectSecp256k1HdWallet,
+// } from "gtk-api";
 
 export async function main() {
   // Initialize the APIClientWrapper
-  const network = "mainnet"; // Replace with the appropriate network
-  const wallet: DirectSecp256k1HdWallet =
-    await DirectSecp256k1HdWallet.fromMnemonic(
-      "mnemonic", // Replace with the appropriate mnemonic
-      {
-        prefix: "sif",
-      }
-    );
-  const client = await APIClientWrapper.create(wallet, network);
-  console.log("APIClientWrapper:", client);
+  // const network = "mainnet"; // Replace with the appropriate network
+  // const wallet: DirectSecp256k1HdWallet =
+  //   await DirectSecp256k1HdWallet.fromMnemonic(
+  //     "mnemonic", // Replace with the appropriate mnemonic
+  //     {
+  //       prefix: "sif",
+  //     }
+  //   );
+  // const client = await APIClientWrapper.create(wallet, network);
+  // console.log("APIClientWrapper:", client);
   // Call placeOrder method
   // const trade = await client.placeOrder(
   //   "uusdc", // collateralTokenType
@@ -32,34 +32,27 @@ export async function main() {
   //   null // limit_price
   // );
   // console.log("Place Order:", trade);
-
   // Call closeOrder method
   // const closedTrade = await client.closeOrder(1615); // Use a mock orderId
   // console.log("Close Order:", closedTrade);
-
   // Call cancelOrder method
   // const cancelledTrade = await client.cancelOrder(1615); // Use a mock orderId
   // console.log("Cancel Order:", cancelledTrade);
-
   // Call getCurrentInterestRate method
   // const interestRate = await client.getCurrentInterestRate("btc");
   // console.log("Current Interest Rate:", interestRate);
-
   // Call getTrades method
   // const trades = await client.getTrades(
   //   TradeDirectionEnum.LONG, // tradeType
   //   TradeStatusEnum.ACTIVE // status
   // );
   // console.log("Get Trades:", trades);
-
   // Call getTrade method
   // const tradeDetails = await client.getTrade(123); // Use a mock tradeId
   // console.log("Get Trade:", tradeDetails);
-
   // Call getTopMatch method
   // const topMatch = await client.getTopMatch("uusdc", 10);
   // console.log("Top Match:", topMatch);
-
   // Call getPnl method
   // const pnl = await client.getPnl(PnlTypeEnum.REALIZED);
   // console.log("PnL:", pnl);
